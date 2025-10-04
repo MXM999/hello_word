cd591a9306fdee9ad32433081186fca82191e705
MXM <morerise@gmail.com>
2025-10-04 18:42:36 +0300

    Add project files.

---
+++ ConsoleApp229/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp229
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+            string[] b = new string[10];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+                fillter(a[i], ref b[i]);
+            }
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.WriteLine(b[i]);
+            }
+        }
+        static void fillter(int numb, ref string m)
+        {
+            if (numb > 5)
+            {
+                m = "good";
+            }
+            else
+            {
+                m = "bad";
+            }
+        }
+    }
+}
